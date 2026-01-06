ffe74c61ac85650b042154cc81ba2dad9f404d6b
Phung Anh Van <phunganhvan13122004@gmail.com>
2026-01-06 10:22:03 +0700

    jsx b30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,12 @@
+import './style.css'
 const VanComponent = () => {
   return (
     <>
       <div>
         Copyright by Van Component
+      </div>
+      <div className="child">
+        Children
       </div>
     </>
   );
