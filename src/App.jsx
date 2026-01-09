f5059d67709b800b85721ee81ff54e1af709775b
Phung Anh Van <phunganhvan13122004@gmail.com>
2026-01-09 08:12:12 +0700

    commit props function #35

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import TodoNew from "./components/todo/TodoNew"
 import imgReact from "./assets/react.svg"
 const App = () =>  {
 
+
+  const addNewTodo = () => {
+    alert("Add new todo");
+  }
+
   return (
     <>
         <div className="todo-container">
           <h1 className="todo-title"> Todo list</h1>
-          <TodoNew/>
+          <TodoNew addNewTodo={addNewTodo} />
           <TodoData/>
-          <img src={imgReact} alt="react logo" className="todo-logo" />
+          <img src={imgReact} alt="react logo" className="todo-logo" /> 
         </div>
       
       
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,9 +1,10 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+    const { addNewTodo } = props;
     return (
         <>
             <div className="todo-add">
                 <input type="text" className="todo-input" placeholder="Enter todo..." />
-                <button className="todo-btn-add"> Add </button>
+                <button className="todo-btn-add" onClick={addNewTodo}> Add </button>
             </div>
         </>
     );
