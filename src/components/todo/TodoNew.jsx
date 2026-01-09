2eb65648633cac59407426e8e5ff1c230d3454d0
Phung Anh Van <phunganhvan13122004@gmail.com>
2026-01-09 08:25:45 +0700

    Dom event #36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,17 @@
 const TodoNew = (props) => {
     const { addNewTodo } = props;
+    const handleClick = () => {
+        addNewTodo();  
+    }
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+    }
     return (
         <>
             <div className="todo-add">
-                <input type="text" className="todo-input" placeholder="Enter todo..." />
-                <button className="todo-btn-add" onClick={addNewTodo}> Add </button>
+                <input type="text" className="todo-input" placeholder="Enter todo..." onChange={(e) => handleChange(e)}/>
+                <button className="todo-btn-add" onClick={handleClick}> Add </button>
             </div>
         </>
     );
