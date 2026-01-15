a98ba8edb61505a9e343ad443c343d2606842750
Phung Anh Van <phunganhvan13122004@gmail.com>
2026-01-15 07:49:09 +0700

    state hoa form # 67

---
+++ src/components/user/user.form.jsx
@@ -0,0 +1,62 @@
+import { Button, Input } from "antd"
+
+
+const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const handleSubmit = (e) => {
+        // Handle form submission logic here
+        
+    }
+    return (
+        <>
+            <div className="user-form" style={{margin: "20px 0px"}}>
+                <div style={{ display: "flex", gap: "15px", flexDirection: "column"}}>
+                    <div>
+                        <span>
+                            Full name:
+                        </span>
+                        <Input 
+                            value={fullName}
+                            onChange={(e) => { setFullName(e.target.value)}}
+                        /> 
+                    </div>
+                    <div>
+                        <span>
+                            Email:
+                        </span>
+                        <Input 
+                            value={email}
+                            onChange={(e) => { setEmail(e.target.value)}}
+                        /> 
+                    </div>
+                    <div>
+                        <span>
+                            Password:
+                        </span>
+                        <Input.Password 
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value)}}
+                        /> 
+                    </div>
+                    <div>
+                        <span>
+                            Phone number:
+                        </span>
+                        <Input 
+                            value={phoneNumber}
+                            onChange={(e) => { setPhoneNumber(e.target.value)}}
+                        /> 
+                    </div>
+                    <div>
+                        <Button type="primary" onClick={() => handleSubmit()}> Create User</Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UserForm;
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,7 +1,14 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
+
 const UserPage = () => {
     return (
         <>
             <h1>User Page</h1>
+            <div style={{ padding: "20px" }}>
+                <UserForm />
+                <UserTable />
+            </div>
         </>
     )
 }
