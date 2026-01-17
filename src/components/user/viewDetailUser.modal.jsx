2d99980f1a64241f477323af080cd6fb935fb539
Phung Anh Van <phunganhvan13122004@gmail.com>
2026-01-17 10:14:14 +0700

    preview image react

--- src/components/user/viewDetailUser.modal.jsx
+++ src/components/user/viewDetailUser.modal.jsx
@@ -1,11 +1,29 @@
 import { Button, Drawer } from 'antd';
+import { useState } from 'react';
 const ViewDetailUserModal = (props) => {
     const { isDetailModalOpen, setIsDetailModalOpen, dataDetail, setDataDetail } = props;
     // console.log("dataDetail: ", dataDetail);
+    const [selectedFile, setSelectedFile] = useState();
+    const [preview, setPreview] = useState();
     const onClose = () => {
         setIsDetailModalOpen(false);
         setDataDetail({});
     };
+    const handleChangeAvatar = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null);
+            setPreview(null);
+            console.log("No file selected");
+            return;
+        }
+        const file = event.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file));
+        }
+        console.log("Selected file: ", file);
+        // handle file upload logic here
+    }
     return (
         <>
             <Drawer
@@ -28,11 +46,11 @@ const ViewDetailUserModal = (props) => {
                 <div style={{ margin: "10px" }}>
                     <p>Phone: {dataDetail.phone}</p>
                 </div>
-                <div>
-                    <p style={{ margin: "10px" }}>Avatar:</p>
+                <p style={{ margin: "10px" }}>Avatar:</p>
+                <div style={{ marginTop: "10px", height: "125px", width: "150px", border: "1px solid #ccc", borderRadius: "8px" }}>
                     <img
-                        style={{ margin: "10px", borderRadius: "8px", border: "1px solid #ccc", objectFit: "cover" }}
-                        height={"250px"} width={"300px"}
+                        style={{ objectFit: "contain" }}
+                        height={"100%"} width={"100%"}
                         src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
                     />
                 </div>
@@ -46,13 +64,21 @@ const ViewDetailUserModal = (props) => {
                             background: "lightblue",
                             borderRadius: "8px",
                             cursor: "pointer"
-                            
+
                         }}
                     >
                         Upload Avatar
                     </label>
-                    <input type="file" name="avatar" hidden id='btnUpload'/>
+                    <input type="file" name="avatar" hidden id='btnUpload' onChange={handleChangeAvatar} />
                 </div>
+                {preview &&
+                    <div style={{ marginTop: "10px", height: "125px", width: "150px", border: "1px solid #ccc", borderRadius: "8px" }}>
+                        <img
+                            style={{ objectFit: "contain" }}
+                            height={"100%"} width={"100%"}
+                            src={`${preview}`}
+                        />
+                    </div>}
             </Drawer>
         </>
     );
